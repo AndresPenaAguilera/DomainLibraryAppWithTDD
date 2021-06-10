ee543c9174c6616c072160eb6c2ca923d715b453
Andrés Peña <p.andres01@hotmail.com>
2021-06-09 21:38:58 -0500

    Test Empty and one element

--- Test/ShoppingCartUnitTest.cs
+++ Test/ShoppingCartUnitTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Test
 {
@@ -8,7 +10,23 @@ namespace Test
         [TestMethod]
         public void TestMethod1()
         {
+            List<object> car = Empty();
+            Assert.IsFalse(car.Any());
+        }
 
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<object> car = Empty();
+            car.Add("Book1");
+            Assert.IsTrue(car.Any());
+        }
+
+       
+
+        private static List<object> Empty()
+        {
+            return new List<object>();
         }
     }
 }
